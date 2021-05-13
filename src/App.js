7ecf89461d55953db08ee5ad23b6d2aefc9873a8
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-13 21:09:20 +0100

    Added footer

--- src/App.js
+++ src/App.js
@@ -3,8 +3,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header"></header>
-      
+    <div className="container">
+     <header className="App-header"></header>
+     <footer>
+
+     </footer>
+      </div>
+      Brain- stormed by <em>Shirin Eldris</em> 🤖, {""}
+    <a href="/"
+     className="purple"
+     target="_blank"
+     rel="noreferrer">
+     Open-Sourced on GitHub
+     </a>
+     <a href="/"
+     className="green"
+     target="_blank"
+     rel="noreferrer">
+     {" "}
+     Netlify hosted
+     </a>
     </div>    
   );
 }

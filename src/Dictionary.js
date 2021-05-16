0d1c569325a8dd5558dba889187f8061c60cba79
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-16 17:23:43 +0100

    Fixed code rendering problems

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -17,7 +17,7 @@ export default function Dictionary() {
 
   //documentation: https://dictionaryapi.dev/
 
-  let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US`+keyword;
+  let apiUrl=`https://api.dictionaryapi.dev/api/v2/entries/en_US/`+keyword;
   console.log(apiUrl); 
   axios.get(apiUrl).then(handleResponse); 
  }

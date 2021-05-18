08eec5ffe2dc9d24db5ca6901c2dea1e432c1c95
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-18 15:01:48 +0100

    Added event listener for phonetics

---
+++ src/Phonetic.js
@@ -0,0 +1,23 @@
+import React from "react";
+//import "./Phonetic.css"; 
+//import ReactAudioPlayer from `react-audio-player`;
+
+export default function Phonetic(props) {
+ console.log(props.phonetic); 
+ let audio = new Audio(props.phonetic.audio);
+ 
+ const start = () => {
+   audio.play()
+ }
+ 
+ return (
+   < div >
+   <button
+    onClick={start}
+    type="button"
+    className="btn btn-outline-warning">Play</button>
+   {" "}
+   <span>{props.phonetic.text}</span>
+   </div >
+ );  
+}

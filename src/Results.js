acbaeed386cb3e4ceb261c40b3431396227ee419
Sibidomaskuli <shildrisno1@gmail.com>
2021-05-19 15:39:06 +0100

    Adding some styling to css files

--- src/Results.js
+++ src/Results.js
@@ -5,23 +5,27 @@ import Phonetic from "./Phonetic";
 export default function Results(props) {
   if (props.results) {
    return (
-    <div className="Results">
-       <h2>{props.results.word}</h2>
-       {props.results.phonetics.map(function (phonetic, index) {
-         return (
-           <div key={index}>
-             <Phonetic phonetic={phonetic} />
-           </div>             
-         );
+    <span>
+     <div className="Results">
+      <h2>{props.results.word}</h2>       
+      {props.results.phonetics.map(function (phonetic, index) {
+       return (
+        <div key={index}>
+         <Phonetic phonetic={phonetic} />
+        </div>        
+       );
       })}
-     {props.results.meanings.map(function (meaning, index) {
-      return (
-       <div key={index}>
-        <Meaning meaning={meaning} />
-       </div>
-      );
-     })}
-    </div>
+      {props.results.meanings.map(function (meaning, index) {
+       return (
+        <section
+         className="meanings"
+         key={index}>
+         <Meaning meaning={meaning} />
+        </section>
+       );
+      })}
+     </div>
+    </span>
    );
   } else {
     return null; 
